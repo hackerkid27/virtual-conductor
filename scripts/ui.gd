6348abb91ec8c4d2ef4ef8de1426c48e453c5df2
= <hackerkid27@users.noreply.github.com>
2025-05-07 10:58:46 -0500

    Create ground-zero for adding functionality

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,13 +1,1 @@
 extends Control
-
-
-signal update_beat
-signal update_tempo
-
-
-func _on_beat_selector_item_selected(index: int) -> void:
-	update_beat.emit()
-
-
-func _on_tempo_spin_box_value_changed(value: float) -> void:
-	update_tempo.emit()
