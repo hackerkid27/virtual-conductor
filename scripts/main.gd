4ff1c620d39e7c2fac7bf38b2c1aa7277ef9a4a4
= <hackerkid27@users.noreply.github.com>
2025-05-12 15:38:42 -0500

    Set up scripts and signals for functionality.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_ui_beat_changed(new_beat: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_ui_tempo_changed(new_tempo: int) -> void:
+	pass # Replace with function body.
