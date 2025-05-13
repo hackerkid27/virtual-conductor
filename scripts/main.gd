4090899d9e60f5d8ed2a18fc1599a752ba7154fe
= <hackerkid27@users.noreply.github.com>
2025-05-12 21:31:38 -0500

    Add functionality to UI

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var animator = $Animation
+@onready var four_beat = animator.get_node("Beat4")
+@onready var three_beat = animator.get_node("Beat3")
+@onready var two_beat = animator.get_node("Beat2")
+@onready var animations = ["beat-4", "beat-3", "beat-2"]
+@onready var animation_nodes = [four_beat, three_beat, two_beat]
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	animator.play("beat-4")
 
 
 func _on_ui_beat_changed(new_beat: int) -> void:
-	pass # Replace with function body.
+	for animation in animation_nodes:
+		animation.hide()
+	animator.current_animation = animations[new_beat]
+	animation_nodes[new_beat].show()
 
 
 func _on_ui_tempo_changed(new_tempo: int) -> void:
-	pass # Replace with function body.
+	var new_tempo_float: float = new_tempo
+	animator.speed_scale = new_tempo_float / 120
